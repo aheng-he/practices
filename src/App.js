0dfb2ca0c59078536772f85a84da1babdb714d02
heyuanheng <13011103682@163.com>
2019-10-22 19:56:56 +0800

    qqq

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import $confirm from './confirm/confirm'
+
+class App extends React.Component {
+  componentDidMount() {
+    $confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+      confirmButtonText: '确定',
+      cancelButtonText: '取消',
+    }).then(() => {
+      alert("点击了确定")
+    }).catch(() => {
+      alert("点击了取消")
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
